639d61a76ee5349bd7f34b20b3f81ec6707dc4ea
Pond <pondwachi.th@gmail.com>
2024-06-07 17:59:32 +0700

    feat: react data feacthing of pond

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [data, setData] = useState("");
+  const [books, setBooks] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${data}`
+      );
+      setBooks(response.data.items);
+    } catch (error) {
+      console.error("Error data not found", error);
+      setBooks([]);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setData(e.target.value);
+    if (e.target.value === "") {
+      setBooks([]);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Search The Books</h1>
+      <div>
+        <input
+          type="text"
+          value={data}
+          onChange={handleInputChange}
+          placeholder="Enter a word here!"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div>
+        {books.length > 0 && (
+          <ul>
+            {books.map((book) => (
+              <li key={book.id}>
+                <h3>📕{book.volumeInfo.title}</h3>
+                <p>🖋️{book.volumeInfo.authors}</p>
+                <p>🌍{book.volumeInfo.publishedDate}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
